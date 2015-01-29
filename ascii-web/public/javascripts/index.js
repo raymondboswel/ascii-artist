3c53af7b70e6c78794b52af00531879533940eeb
Raymond Boswel <raymondboswel@gmail.com>
2015-01-29 13:26:02 -0800

    Major update, sortof working now. Needs some cleanup

--- ascii-web/public/javascripts/index.js
+++ ascii-web/public/javascripts/index.js
@@ -1,20 +1,110 @@
 
-
 var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-2.1.3.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
+console.log("Running index.js");
 
 
+var clck = function() {
+    asciiArt = document.getElementById("asciiArt");
+    fileUpload = document.getElementById("fileUpload");
+    var formData = new FormData();
+    formData.append("userFile", fileUpload.files[0]);
+//    formData.append("file", fileUpload.files[0]);
+    console.log(fileUpload.files[0]);
+
+    $.ajax({
+      url : "/", 
+      type : "POST",
+      data : formData, 
+      processData : false,
+      contentType : false,
+      success : function(data, textStatus, jqXHR) {
+	console.log(data);
+        asciiArt.innerHTML = data;
+      },
+      error: function(data, textStatus, jqXHR) {
+          console.log(jqXHR);
+          console.log("fuuuuc")
+      }
+    });
+
+    /*$.post( "/", function(data) {
+      console.log(data);
+      asciiArt.innerHTML = data;
+    });*/
+}   
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////
+
+/*var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-2.1.3.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+*/
+/*var clck = function() {
+    asciiArt = document.getElementById("asciiArt");
+    $.post( "/", function(data) {
+      console.log(data);
+      asciiArt.innerHTML = data;
+    });
+}   */
+
+/*
   console.log("Running index.js");
 
 
 var clck = function() {
     asciiArt = document.getElementById("asciiArt");
-    $.post( "/", function(data) {
+    var fileUpload = document.getElementById("fileUpload");
+    var file = fileUpload.files[0];
+    console.log("File upload: " + file );
+
+    var formData = new FormData();
+    formData.append("file", file);
+    formData.append("testString", "test");
+    console.log("form data:" + formData);
+
+/*    for(var propName in formData) {
+        propValue = formData[propName];
+        console.log(propName, propValue);
+        }
+
+$.ajax({
+    url : "/",
+    type: "POST",
+    data : formData,
+    processData: false,
+    contentType: "multipart/form-data",
+    success:function(data, textStatus, jqXHR){        
+        console.log(data);
+        asciiArt.innerHTML = data;
+    },    
+    error: function(jqXHR, textStatus, errorThrown){
+        console.log("It failed :/");
+    }
+});*/
+
+
+/*
+    $.post( "/", formData, function(data) {
         console.log(data);
         asciiArt.innerHTML = data;
     });
+    
+/*    for(var propName in uploadFile) {
+        propValue = uploadFile[propName];
+        console.log(propName, propValue);
+        }*/
+ 
 
-
-    }
+    
--- ascii-web/routes/index.js
+++ ascii-web/routes/index.js
@@ -2,21 +2,47 @@ var express = require('express');
 var router = express.Router();
 
 var fs = require('fs');
-var app = require('../app');
+var app = express();
+multer  = require('multer')
+
+var busboy = require('connect-busboy');
+//...
+
+
+
+
 var resolutions = {"4":"4"};
 
 console.log("Server side");
 
 router.post('/', function(req, res) {
 //Should use async, using sync as first iteration. This sortof works, but page load fails for a bit while execution is taking place.
+    var propValue;
+    console.log(req);
+    console.log(req.files.userFile.path);
+    partialPath = req.files.userFile.path;
+    console.log(__dirname);
+    pathElements = __dirname.split('/');
+    newPath="";
+    for(i=0; i<pathElements.length -1; i++)
+    {
+       newPath = newPath +'/' +pathElements[i];
+     }
+    newPath = newPath+'/'+partialPath;
+    newPath = newPath.substring(1, newPath.length)
+    
+    console.log(newPath);
     var execSync = require ('exec-sync');
-    var v = execSync('python /home/dev/python_project/ascii_generator.py /home/dev/python_project/Bob_Marley.png 4 /home/dev/python_project/nodeOutput.txt' );
+//    var v = execSync('python /home/dev/python_project/ascii_generator.py /home/dev/python_project/Bob_Marley.png 4 /home/dev/python_project/nodeOutput.txt' );
+    var v = execSync('python /home/dev/python_project/ascii_generator.py ' + newPath +' 4 /home/dev/python_project/nodeOutput.txt' );
     text = fs.readFileSync('/home/dev/python_project/nodeOutput.txt');
-    console.log("posting....");
+    //console.log("posting...." + text);
    // text = "post";
     res.send(text);
 
 });
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   text = "Result will be displayed here";
